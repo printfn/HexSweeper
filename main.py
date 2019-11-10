63d5b904c0bb17a322307681dbdf847a1d396793
printfn <printfn@users.noreply.github.com>
2019-11-11 00:44:36 +0100

    Show error if using Python older than 3.6

--- main.py
+++ main.py
@@ -4,6 +4,12 @@
 Entry-point for HexSweeper.
 """
 
+import sys
+
+if sys.version_info < (3,6):
+    print("Sorry, this program requires at least Python 3.6.")
+    sys.exit(1)
+
 from game_ui import GameUI
 from difficulty import Difficulty
 
