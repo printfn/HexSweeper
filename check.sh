8b2bf4d82309c67670194248770aef98579572df
printfn <printfn@users.noreply.github.com>
2019-11-11 00:58:21 +0100

    Fix pylint warnings

--- check.sh
+++ check.sh
@@ -2,4 +2,3 @@
 
 mypy .
 pylint *.py
-
--- main.py
+++ main.py
@@ -6,10 +6,11 @@ Entry-point for HexSweeper.
 
 import sys
 
-if sys.version_info < (3,6):
+if sys.version_info < (3, 6):
     print("Sorry, this program requires at least Python 3.6.")
     sys.exit(1)
 
+# pylint: disable=wrong-import-position
 from game_ui import GameUI
 from difficulty import Difficulty
 
